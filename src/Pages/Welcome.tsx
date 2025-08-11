16216452b6133f9bc6d80959d3612b813cd214d8
Steven Mueller <steven91muellermueller@web.de>
2025-08-11 13:12:00 +0200

    quotes working well, needs to be bound checked

--- src/Pages/Welcome.tsx
+++ src/Pages/Welcome.tsx
@@ -1,13 +1,18 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import "../Styles/Welcome.css";
 
 export default function Welcome() {
   const apiUrl = import.meta.env.VITE_API_QUOTES;
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState(null);
 
   async function fetchData() {
     try {
       const response = await fetch(apiUrl);
-      const data = await response.json();
-      localStorage.setItem("quotes", JSON.stringify(data));
+      const resData = await response.json();
+      localStorage.setItem("quotes", JSON.stringify(resData));
+      localStorage.setItem("quote_index", index.toString());
+      setData(resData);
     } catch (error) {
       console.log("Failed to receive data", error);
     }
@@ -20,16 +25,48 @@ export default function Welcome() {
 
     if (!localStorage.getItem("quotes")) {
       fetchData();
+    } else {
+      setIndex(Number(localStorage.getItem("quote_index")));
+      setData(JSON.parse(localStorage.getItem("quotes") || "[]"));
     }
-  }, []);
+
+    const handleBeforeUnload = () => {
+      const incrementIndex = index + 1;
+      localStorage.setItem("quote_index", incrementIndex.toString());
+    };
+
+    window.addEventListener("beforeunload", handleBeforeUnload);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleBeforeUnload);
+    };
+  }, [index]);
 
   return (
-    <div className="flex flex-col gap-10">
-      <h1 className="font-extrabold underline text-4xl">Welcome to HRDirect</h1>
-      <p>
-        Quote of the day:
-        {JSON.parse(localStorage.getItem("quotes") || "[]")[0]?.q}
-      </p>
+    <div className="flex flex-col gap-10 text-center">
+      <h1 className="font-extrabold text-4xl text-shadow-lg text-shadow-black/50">
+        Welcome to HRDirect
+      </h1>
+      <div className="flex flex-row w-full h-1/5 gap-10">
+        <p className="font-extrabold text-shadow-lg text-shadow-black/50">
+          Quote of the day:
+        </p>
+        <blockquote className="quote text-right">
+          <i>
+            "
+            {data &&
+              JSON.parse(localStorage.getItem("quotes") || "[]")[index]?.q}
+            "
+            <br />
+            <br />
+            <span className="text-shadow-lg text-shadow-black/50 mr-3">
+              -{" "}
+              {data &&
+                JSON.parse(localStorage.getItem("quotes") || "[]")[index]?.a}
+            </span>
+          </i>
+        </blockquote>
+      </div>
     </div>
   );
 }
